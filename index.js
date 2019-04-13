5463061c15695524f63426d68057ec51e3182849
Aaron White <aaron@singularity.vc>
2019-04-12 21:09:13 -0400

    cache behavior achieved

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const bodyParser = require('body-parser')
-
+const hash = require('object-hash');
+const interceptor = require('express-interceptor');
 const { ApolloServer, gql } = require('apollo-server-express');
+
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr;
+  if (this.length === 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
 
 const {
   makeRemoteExecutableSchema,
@@ -32,24 +44,45 @@ async function main () {
   // parse application/json
   app.use(bodyParser.json())
 
+  const objReplaceByHash = {}
 
   app.use(function(req,res,next){
-    console.log({body:req.body})
 
-    if(req.body.query && req.body.query == 'query {\n  loginURL\n}') {
-      console.log("Woot")
-      res.setHeader('Content-Type', 'application/json')
-      res.end(JSON.stringify({
-        data: {
-          loginURL: "foo"
-        }
-      }
-        , null, 2))
+    if(!req.body || !req.body.query) {
+      console.log("Not GQL query")
+      next()
     }
-    else{
+
+    req.queryHash = req.body.query.hashCode()
+    console.log("hashes as ", req.queryHash)
+
+    if(objReplaceByHash[req.queryHash]) {
+      const hashValue = objReplaceByHash[req.queryHash]
+      console.log("Returning from hash cache")
+      res.setHeader('Content-Type', 'application/json')
+      res.end(hashValue)
+    }
+    else {
       next()
     }
   })
+  
+  // Add the interceptor middleware
+  app.use(interceptor(function(req, res){
+    return {
+      // Only HTML responses will be intercepted
+      isInterceptable: function(){
+        console.log(res.get('Content-Type'))
+        return /application\/json/.test(res.get('Content-Type'));
+      },
+      // Appends a paragraph at the end of the response body
+      intercept: function(body, send) {
+        console.log("Intercepting repsonse & caching")
+        objReplaceByHash[req.queryHash] = body
+        send(body);
+      }
+    };
+  }));
 
   server.applyMiddleware({
     app: app
